1272215be3dce8a83e8131bb72d3f45ebaee0a3b
Mica <25252229+MicaLovesKPOP@users.noreply.github.com>
2022-11-29 15:01:26 +0100

    Create script.js

---
+++ beta2.1/script.js
@@ -0,0 +1,99 @@
+var li = $('li');
+var liSelected;
+$(window).keydown(function(e) {
+    if(e.which === 40) {
+        if(liSelected) {
+            liSelected.removeClass('selected');
+            next = liSelected.next();
+            if(next.length > 0) {
+                liSelected = next.addClass('selected');
+            } else {
+                liSelected = li.eq(0).addClass('selected');
+            }
+        } else {
+            liSelected = li.eq(0).addClass('selected');
+        }
+    } else if(e.which === 38) {
+        if(liSelected) {
+            liSelected.removeClass('selected');
+            next = liSelected.prev();
+            if(next.length > 0) {
+                liSelected = next.addClass('selected');
+            } else {
+                liSelected = li.eq(4).addClass('selected');
+            }
+        } else {
+            liSelected = li.last().addClass('selected');
+        }
+    }
+});
+
+function openTools() {
+    var x = document.getElementById("tools");
+    var z = document.getElementById("main");
+    z.style.display = "none";
+    x.style.display = "flex";
+//    next = liSelected.next();
+//    liSelected = next.addClass('selected');
+//    liSelected = li.eq(0).addClass('selected');
+}
+
+function openResources() {
+    var x = document.getElementById("resources");
+    var z = document.getElementById("main");
+    z.style.display = "none";
+    x.style.display = "flex";
+}
+
+function openOptions() {
+    var x = document.getElementById("options");
+    var z = document.getElementById("main");
+    z.style.display = "none";
+    x.style.display = "flex";
+}
+
+$(window).mouseover(function() {
+    if(liSelected) {
+        liSelected.removeClass('selected');
+        current = liSelected.current();
+        liSelected = current.addClass('selected');
+    }
+});
+
+$(document).keypress(function(e) {
+    if (e.which == 13) {
+        $('li.selected a').trigger('click');
+    }
+});
+
+var element = $('div.flexbox-main');
+function trapFocus(element) {
+    var focusableEls = element.querySelectorAll('a[href]:not([disabled])');
+    var firstFocusableEl = focusableEls[0];  
+    var lastFocusableEl = focusableEls[focusableEls.length - 1];
+    var KEYCODE_TAB = 9;
+  
+    element.addEventListener('keydown', function(e) {
+        var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+  
+        if (!isTabPressed) { 
+            alert('Tab key pressed');
+            return; 
+        }
+  
+        if ( e.shiftKey ) /* shift + tab */ {
+            if (document.activeElement === firstFocusableEl) {
+                lastFocusableEl.focus();
+                e.preventDefault();
+            }
+        } else /* tab */ {
+        if (document.activeElement === lastFocusableEl) {
+            firstFocusableEl.focus();
+            e.preventDefault();
+            }
+        }
+    });
+}
+
+// Makes escape key function as 'page back' function
+window.addEventListener("keyup", function(e){ if(e.keyCode == 27) history.back(); }, false);
